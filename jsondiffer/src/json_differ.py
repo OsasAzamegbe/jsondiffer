d9d5122e6f2d3ebb64c889ed629179a8a912d90d
Osamudiamen <osas.azamegbe@gmail.com>
2022-04-23 13:38:40 +0100

    format document

--- jsondiffer/src/json_differ.py
+++ jsondiffer/src/json_differ.py
@@ -2,6 +2,8 @@ from typing import Dict, List, Union, Any
 
 
 class JsonDiffer(object):
-    def __init__(self, json_a: Union[Dict, List] = None, json_b: Union[Dict, List] = None) -> None:
+    def __init__(
+        self, json_a: Union[Dict, List] = None, json_b: Union[Dict, List] = None
+    ) -> None:
         self.json_a = json_a
         self.json_b = json_b
