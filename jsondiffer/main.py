7fae13ed6d46ed9fcaed469a4e37b33f71d736e6
Osamudiamen <osas.azamegbe@gmail.com>
2022-04-24 14:10:29 +0100

    feat: add logic for json object recursive diffing

--- jsondiffer/main.py
+++ jsondiffer/main.py
@@ -1,5 +1,6 @@
-from jsondiffer.src.json_differ import JsonDiffer
+from src.json_differ import JsonDiffer
 
 
-if __name__ != "__main__":
-    pass
+if __name__ == "__main__":
+    differ = JsonDiffer({"1": 2}, {"1": 2})
+    differ.generate_diffs()
--- jsondiffer/src/json_differ.py
+++ jsondiffer/src/json_differ.py
@@ -1,28 +1,89 @@
+from datetime import datetime
 from typing import Callable, Union, Dict, List
+from dataclasses import dataclass
+from enum import Enum
 import json
 
 
 JsonType = Union[Dict, List]
+PrimitiveDataType = Union[str, bool, int, float, datetime, None]
+
+
+class DiffEnum(Enum):
+    MISSING_LEFT = 1
+    MISSING_RIGHT = 2
+    MISMATCHED = 3
+
+
+@dataclass
+class Diff(object):
+    diff_type: DiffEnum
+    key: Union[str, int]
 
 
 class JsonDiffer(object):
     def __init__(self, json_a: JsonType = None, json_b: JsonType = None) -> None:
         self.json_a = json_a if json_a is not None else {}
         self.json_b = json_b if json_b is not None else {}
+        self.diff_list: List[Diff] = []
 
     @staticmethod
     def _is_json_loadable(
-        load_function: Callable, json_data: Union[str, JsonType], *args, **kwargs
+        load_function: Callable, json_data: str, *args, **kwargs
     ) -> bool:
         try:
             load_function(json_data, *args, **kwargs)
-        except ValueError as error:
+        except ValueError:
             return False
         return True
 
     @staticmethod
-    def is_valid_json(json_data: Union[str, JsonType], is_file: bool = False) -> bool:
+    def is_valid_json(json_data: str, is_file: bool = False) -> bool:
         if is_file:
             with open(json_data, "r") as json_file:
                 return JsonDiffer._is_json_loadable(json.load, json_file)
         return JsonDiffer._is_json_loadable(json.loads, json_data)
+
+    @staticmethod
+    def _is_mismatched(
+        json_a: Union[JsonType, PrimitiveDataType],
+        json_b: Union[JsonType, PrimitiveDataType],
+    ) -> bool:
+        return type(json_a) is not type(json_b) or (
+            isinstance(json_a, PrimitiveDataType) and json_a != json_b
+        )
+
+    def generate_diffs(self):
+        self._diff_node(self.json_a, self.json_b)
+        print("Diffs generated:", self.diff_list)
+
+    def _diff_node(
+        self,
+        json_a: Union[JsonType, PrimitiveDataType],
+        json_b: Union[JsonType, PrimitiveDataType],
+        prev_key: str = "",
+    ):
+        if self._is_mismatched(json_a, json_b):
+            self.diff_list.append(Diff(DiffEnum.MISMATCHED, prev_key))
+            return
+
+        if isinstance(json_a, dict):
+            longer_iterable = json_b if len(json_a) < len(json_b) else json_a
+            for key in longer_iterable.keys():
+                if key not in json_a:
+                    self.diff_list.append(Diff(DiffEnum.MISSING_LEFT, key))
+                    continue
+                elif key not in json_b:
+                    self.diff_list.append(Diff(DiffEnum.MISSING_RIGHT, key))
+                    continue
+                self._diff_node(json_a[key], json_b[key], key)
+        elif isinstance(json_a, list):
+            larger_len = len(json_b) if len(json_a) < len(json_b) else len(json_a)
+            for idx in range(larger_len):
+                if idx >= len(json_a):
+                    self.diff_list.append(Diff(DiffEnum.MISSING_LEFT, idx))
+                    continue
+                elif idx >= len(json_b):
+                    self.diff_list.append(Diff(DiffEnum.MISSING_RIGHT, idx))
+                    continue
+                self._diff_node(json_a[idx], json_b[idx], idx)
